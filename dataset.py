e31aa9d726dfc3c72f1922343aab3f3b6220bfe5
carloscalvin <carloscalvinp@gmail.com>
2026-02-01 00:04:37 +0100

    Add dataset

---
+++ dataset.py
@@ -0,0 +1,59 @@
+import os
+import cv2
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
+class BirdDataset(Dataset):
+    def __init__(self, df, root_dir, transform=None):
+        self.df = df
+        self.root_dir = root_dir
+        self.transform = transform
+        self.file_names = df['filename'].values
+        self.labels = df['cls'].values
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        file_name = self.file_names[idx]
+        file_path = os.path.join(self.root_dir, file_name)
+        image = cv2.imread(file_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        if self.transform:
+            augmented = self.transform(image=image)
+            image = augmented['image']
+        label = torch.tensor(self.labels[idx], dtype=torch.long)
+        return image, label
+
+def get_transforms(data='train', img_size=224):
+    resize_target = int(img_size * 1.15)
+    if data == 'train':
+        return A.Compose([
+            A.SmallestMaxSize(max_size=resize_target),
+            A.RandomCrop(height=img_size, width=img_size),
+            A.HorizontalFlip(p=0.5),
+            A.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.05, rotate_limit=15, p=0.5),
+            A.CoarseDropout(max_holes=8, max_height=16, max_width=16, p=0.2),
+            A.Normalize(
+                mean=[0.485, 0.456, 0.406], 
+                std=[0.229, 0.224, 0.225], 
+                max_pixel_value=255.0, 
+                p=1.0
+            ),
+            ToTensorV2(),
+        ])
+    elif data == 'valid':
+        return A.Compose([
+            A.SmallestMaxSize(max_size=resize_target),
+            A.CenterCrop(height=img_size, width=img_size),
+            A.Normalize(
+                mean=[0.485, 0.456, 0.406], 
+                std=[0.229, 0.224, 0.225], 
+                max_pixel_value=255.0, 
+                p=1.0
+            ),
+            ToTensorV2(),
+        ])
